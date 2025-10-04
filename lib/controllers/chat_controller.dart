ffded997a036ea8680b0cb1759c258a789d65b47
nivuduc2191999 <nivuduc2191999@gmail.com>
2025-10-04 02:23:42 +0000

    fix: Sửa lỗi tạo cuộc trò chuyện và logic đếm tin nhắn chưa đọc

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,79 +1,80 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/consts/firebase_consts.dart';
 
 class ChatController extends GetxController {
-  late CollectionReference chats;
-
   var friendName = Get.arguments[0];
   var friendId = Get.arguments[1];
 
-  var senderName = ''.obs;
-  var currentId = ''.obs;
+  late String senderName;
+  var currentId = auth.currentUser!.uid;
 
   final messageController = TextEditingController();
 
   dynamic chatDocId;
-
   var isLoading = false.obs;
 
   @override
   void onInit() {
     super.onInit();
-    chats = firestore.collection(chatsCollection);
-    getChatId().then((value) {
-      isLoading.value = false;
+    isLoading.value = true;
+    getSenderName().then((_) {
+      getChatId().then((_) {
+        isLoading.value = false;
+      });
     });
   }
 
-  getChatId() async {
-    isLoading.value = true;
-    await chats
-        .where('users', isEqualTo: {friendId: null, currentId.value: null})
-        .limit(1)
-        .get()
-        .then((QuerySnapshot snapshot) {
-          if (snapshot.docs.isNotEmpty) {
-            chatDocId = snapshot.docs.single.id;
-          } else {
-            chats
-                .add({
-                  'users': {friendId: null, currentId.value: null},
-                  'friend_name': friendName,
-                  'sender_name': senderName.value,
-                  // Initialize fields for the new chat
-                  'last_msg': '',
-                  'last_msg_time': FieldValue.serverTimestamp(),
-                  'admin_unread_count': 0,
-                })
-                .then((value) => {chatDocId = value});
-          }
-        });
+  getSenderName() async {
+    var userDoc = await firestore.collection(usersCollection).doc(currentId).get();
+    if (userDoc.exists) {
+      senderName = userDoc.data()?['name'] ?? 'Unknown User';
+    } else {
+      senderName = 'Unknown User';
+    }
   }
 
-  sendMessage(String msg) async {
-    if (msg.trim().isNotEmpty) {
-      chats.doc(chatDocId).collection(messagesCollection).add({
-        'created_on': FieldValue.serverTimestamp(),
-        'msg': msg,
-        'uid': currentId.value,
-      });
+  getChatId() async {
+    List<String> sortedIds = [currentId, friendId];
+    sortedIds.sort();
+    chatDocId = sortedIds.join('_');
 
-      chats.doc(chatDocId).update({
-        'last_msg': msg,
+    var chatDocumentRef = firestore.collection(chatsCollection).doc(chatDocId);
+    var docSnapshot = await chatDocumentRef.get();
+
+    if (!docSnapshot.exists) {
+      await chatDocumentRef.set({
+        'created_on': FieldValue.serverTimestamp(),
+        'last_msg': '',
         'last_msg_time': FieldValue.serverTimestamp(),
-        'admin_unread_count': FieldValue.increment(1),
+        'users': [currentId, friendId],
+        'friend_name': friendName,
+        'sender_name': senderName,
+        'admin_unread_count': 0,
+        'user_unread_count': 0,
       });
     }
   }
 
-  void markMessagesAsRead(String chatDocId) {
-    var chatRef = chats.doc(chatDocId).collection(messagesCollection);
-    chatRef.where('uid', isNotEqualTo: currentId.value).get().then((snapshot) {
-      for (var doc in snapshot.docs) {
-        doc.reference.update({'read': true});
-      }
+  sendMessage(String msg) async {
+    if (msg.trim().isEmpty) return;
+
+    var chatDocumentRef = firestore.collection(chatsCollection).doc(chatDocId);
+
+    await chatDocumentRef.collection(messagesCollection).add({
+      'created_on': FieldValue.serverTimestamp(),
+      'msg': msg,
+      'uid': currentId,
+    });
+
+    const adminId = 'QsoApR4yrPSCQzLOxcagt26k38n2';
+
+    await chatDocumentRef.update({
+      'last_msg': msg,
+      'last_msg_time': FieldValue.serverTimestamp(),
+      if (currentId != adminId) 'admin_unread_count': FieldValue.increment(1),
+      if (currentId == adminId) 'user_unread_count': FieldValue.increment(1),
     });
   }
 }
